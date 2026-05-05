1c04ca11661f52e538f7521b70a77b5b5bfd9226
Gabriel Luis de Santana <131803001+gabrieldsantana28@users.noreply.github.com>
2026-05-05 11:25:50 -0300

    Adiciona Aula do Módulo Math

--- Aulas/matematica.py
+++ Aulas/matematica.py
@@ -1,11 +1,26 @@
 # Funções builtin - Internas
+import math as m
 
-valores = [2,5,8,9,4,6,2,9,0]
-print(max(valores))
-print(min(valores))
-a = -5
-b = 4
-print(abs(a))
-print(pow(a,b))
-c = 2.789421
-print(round(c,2)) 
+# valores = [2,5,8,9,4,6,2,9,0]
+# print(max(valores))
+# print(min(valores))
+# a = -5
+# b = 4
+# print(abs(a))
+# print(pow(a,b))
+# c = 2.789421
+# print(round(c,2)) 
+
+x, y = 8, 100
+
+# raiz_quadrada = m.sqrt(x)
+# print(round(raiz_quadrada,2))
+# print(m.ceil(raiz_quadrada))
+# print(m.floor(raiz_quadrada))
+
+# logaritmo = m.log10(y)
+# print(logaritmo)
+
+print(m.pi)
+print(m.factorial(x))
+print(x/m.inf)
