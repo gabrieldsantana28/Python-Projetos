8fa6f101ec775e4e0307c8d33aabb64e990410af
Gabriel Luis de Santana <131803001+gabrieldsantana28@users.noreply.github.com>
2026-05-06 09:43:45 -0300

    Adicionar Aula de POO

---
+++ Aulas/poo.py
@@ -0,0 +1,69 @@
+# Orientação a Objetos: Paradigma de Programação
+# Classes e Objetos
+
+class Veiculo:
+    def movimentar(self):
+        print(f"Sou um veículo e me desloco!")
+    
+    def __init__(self, fabricante, modelo):
+        self.__fabricante = fabricante
+        self.__modelo = modelo
+        self.__num_registro = None
+
+    # Setter
+    def set_num_registro(self, registro):
+        self.__num_registro = registro
+
+    # Getter
+    def get_fabr_modelo(self):
+        print(f'Modelo: {self.__modelo}, Fabricante: {self.__fabricante}.\n')
+
+    def get_num_registro(self):
+        return self.__num_registro
+    
+class Carro(Veiculo):
+    # Método __init__ será herdado
+    def movimentar(self):
+        print(f"Sou um carro e ando pelas ruas!")
+
+class Motocicleta(Veiculo):
+    def movimentar(self):
+        print(f"Corro muito!")
+
+class Aviao(Veiculo):
+    def __init__(self, fabricante, modelo, categoria):
+        self.__cat = categoria
+        super().__init__(fabricante, modelo)
+
+    def movimentar(self):
+        print("Eu voou alto!")
+
+    def get_categoria(self):
+        return self.__cat
+
+if __name__ == "__main__":
+    # meu_veiculo = Veiculo("GM", "Cadillac Escalade")
+    # meu_veiculo.movimentar()
+    # meu_veiculo.get_fabr_modelo()
+    # meu_veiculo.set_num_registro(49024)
+    # print(f'Registro: {meu_veiculo.get_num_registro()}\n')
+
+    # meu_carro = Carro("Volkswagen", "Polo")
+    # meu_carro.movimentar()
+    # meu_carro.get_fabr_modelo()
+    # print(f"Registro: {meu_carro.get_num_registro()}")
+
+    # seu_carro = Carro("Ford", "Ford Ka")
+    # seu_carro.movimentar()
+    # seu_carro.get_fabr_modelo()
+    # seu_carro.set_num_registro(34823)
+    # print(f"Registro: {seu_carro.get_num_registro()}")
+
+    # moto = Motocicleta("Kawasaki", "Ninja")
+    # moto.movimentar()
+    # moto.get_fabr_modelo()
+
+    meu_aviao = Aviao("Boing", "747", "Comercial")
+    meu_aviao.movimentar()
+    meu_aviao.get_fabr_modelo()
+    print(f'Categoria: {meu_aviao.get_categoria()}')
